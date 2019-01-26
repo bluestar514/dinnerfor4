4a819eb25f7873acec84bf858cf62ce5f8efe05b
Beth <elaolive@ucsc.edu>
2019-01-26 13:51:42 -0800

    dialogue system functions
    Method exists to open and close dialogue boxes (activate and deactivate the boxes).
    Typewritter effect on text.

--- GGJ2019 Dinner for 4/Assets/Scripts/DialogueBox.cs
+++ GGJ2019 Dinner for 4/Assets/Scripts/DialogueBox.cs
@@ -10,32 +10,67 @@ public class DialogueBox : MonoBehaviour
     public List<string> content;
     int page = 0;
 
+    public float textSpeed = .01f; //seconds per character
+    bool effectActive = false;
+
     public GameObject speakerNameObj; 
     public GameObject textObject;
     Text nameText;
     Text dialogueText;
 
-    private void Start() {
+    
+
+    private void Awake() {
         nameText = speakerNameObj.GetComponent<Text>();
         dialogueText = textObject.GetComponent<Text>();
 
-        nameText.text = speakerName;
-        dialogueText.text = content[page];
+        OpenWindow();
     }
 
     private void Update() {
         if(Input.GetMouseButtonDown(0)){
-            page++;
-            if(page < content.Count){
+            if(effectActive){
+                effectActive = false;
                 dialogueText.text = content[page];
             }else{
-                CloseWindow();
+                page++;
+                if(page < content.Count){
+                    StartTypewritterEffect(content[page], dialogueText);
+                }else{
+                    CloseWindow();
+                }
             }
         }
     }
 
-    private void CloseWindow(){
-        Destroy(gameObject);
+    public void OpenWindow(){
+        page = 0;
+        nameText.text = speakerName;
+        StartTypewritterEffect(content[page], dialogueText);
+        //dialogueText.text = content[page];
+    }
+
+    public void CloseWindow(){
+        gameObject.SetActive(false);
+    }
+
+    public void StartTypewritterEffect(string dialogue, Text text){
+        effectActive = true;
+
+        IEnumerator coroutine = TypewritterEffect(dialogue, text);
+        StartCoroutine(coroutine);
+    }
+
+    private IEnumerator TypewritterEffect(string dialogue, Text text){
+        text.text = "";
+        foreach(char c in dialogue){
+            if(!effectActive) break;
+
+            text.text += c;
+            yield return new WaitForSeconds(textSpeed);            
+        }
+
+        effectActive = false;
     }
 
 }
---
+++ GGJ2019 Dinner for 4/Assets/Scripts/Speaker.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Speaker : MonoBehaviour
+{
+    public List<string> content;
+    public string speakerName;
+    public GameObject dialogueBoxPrefab;
+    GameObject dialogueBoxObj;
+
+    public GameObject canvas; 
+
+
+    private void Start() {
+        dialogueBoxObj = Instantiate(dialogueBoxPrefab, canvas.transform);
+        DialogueBox box = dialogueBoxObj.GetComponent<DialogueBox>();
+        box.speakerName = speakerName;
+        box.content = content;
+
+        box.CloseWindow();
+    }
+
+    private void Update() {
+        if(Input.GetKeyDown(KeyCode.T)){
+            OpenDialogueBox();
+        }
+    }
+
+    public void OpenDialogueBox(){
+        dialogueBoxObj.SetActive(true);
+        dialogueBoxObj.GetComponent<DialogueBox>().OpenWindow();
+    }
+}
