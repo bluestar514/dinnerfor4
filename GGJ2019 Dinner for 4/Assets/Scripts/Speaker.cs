ef3399ff73a7376581a6ad4ca5123ef583d7e434
Beth <elaolive@ucsc.edu>
2019-01-26 14:31:03 -0800

    Added Converstation Class
    Allows better grouping of dialogue bits

--- GGJ2019 Dinner for 4/Assets/Scripts/Speaker.cs
+++ GGJ2019 Dinner for 4/Assets/Scripts/Speaker.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class Speaker : MonoBehaviour
 {
-    public List<string> content;
+    //public List<string> content;
+    public List<Conversation> conversations;
     public string speakerName;
+    int conversationNum=0;
+
     public GameObject dialogueBoxPrefab;
     GameObject dialogueBoxObj;
 
@@ -16,7 +19,7 @@ public class Speaker : MonoBehaviour
         dialogueBoxObj = Instantiate(dialogueBoxPrefab, canvas.transform);
         DialogueBox box = dialogueBoxObj.GetComponent<DialogueBox>();
         box.speakerName = speakerName;
-        box.content = content;
+        
 
         box.CloseWindow();
     }
@@ -29,6 +32,24 @@ public class Speaker : MonoBehaviour
 
     public void OpenDialogueBox(){
         dialogueBoxObj.SetActive(true);
+        
+        DialogueBox box = dialogueBoxObj.GetComponent<DialogueBox>();
+        Conversation current = conversations[conversationNum];
+        
+        box.content = current.content;
+        if(!current.repeated) conversationNum += 1;
+        if(conversationNum >= conversations.Count) conversationNum = 0;
+        if(current.absoluteNumber > 0) current.absoluteNumber--;
+        if(current.absoluteNumber == 0) conversations.Remove(current);
+        
+
         dialogueBoxObj.GetComponent<DialogueBox>().OpenWindow();
     }
+}
+
+[System.Serializable]
+public class Conversation{
+    public List<string> content;
+    public bool repeated;
+    public int absoluteNumber = -1;
 }
