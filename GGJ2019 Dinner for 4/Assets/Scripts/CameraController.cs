c9f89ba21331a2d0d0ec7cb0457cc42ff2f9040d
JosephBoudeman <jboudema@ucsc.edu>
2019-01-25 22:07:18 -0800

    Charcter Control and Camera Look
    Fixed character control and camera

--- GGJ2019 Dinner for 4/Assets/Scripts/CameraController.cs
+++ GGJ2019 Dinner for 4/Assets/Scripts/CameraController.cs
@@ -4,21 +4,33 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    GameObject character;
-    Vector2 mouseLook;
-    Vector2 smoothCam;
-    public float sens = 7f;
-    public float smooth = 3f;
 
-    // Start is called before the first frame update
+    public Transform playerRoot, lookRoot;
+    public bool invert;
+    public float sensivity = 5f;
+    public Vector2 default_Look_Limits = new Vector2(-70f, 80f);
+
+    private Vector2 look_Angles;
+    private Vector2 current_Mouse_Look;
+
+    // Use this for initialization
     void Start()
     {
-        character = transform.parent.gameObject;//set this camera as the child of gameobject
+
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        Vector2 = 
+        current_Mouse_Look = new Vector2(Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"));
+
+        look_Angles.x += current_Mouse_Look.x * sensivity * (invert ? 1f : -1f);
+        look_Angles.y += current_Mouse_Look.y * sensivity;
+
+        look_Angles.x = Mathf.Clamp(look_Angles.x, default_Look_Limits.x, default_Look_Limits.y);
+
+        lookRoot.localRotation = Quaternion.Euler(look_Angles.x, 0f, 0f);
+        playerRoot.localRotation = Quaternion.Euler(0f, look_Angles.y, 0f);
     }
-}
+} 
+
--- GGJ2019 Dinner for 4/Assets/Scripts/PlayerController.cs
+++ GGJ2019 Dinner for 4/Assets/Scripts/PlayerController.cs
@@ -2,15 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterController : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     public float speed = 8f;
-    public float gravity = -9.8f;
+    public float customGravity = 9.8f;
     float horizontal;
     float vertical;
     Vector3 movement;
 
-    CharacterController controller;
+    private CharacterController controller;
+    private Vector3 playerVelocity;
 
     // Start is called before the first frame update
     void Start()
@@ -28,10 +29,14 @@ public class CharacterController : MonoBehaviour
             Cursor.lockState = CursorLockMode.Locked;//cursor comes back
         }
 
-        movement = new Vector3(Input.GetAxis("Vertical"), 0, (Input.GetAxis("Horizontal")));
+        movement = new Vector3(Input.GetAxis("Horizontal"), 0f, (Input.GetAxis("Vertical")));
         movement *= speed;//sets speed of movement
         movement = Vector3.ClampMagnitude(movement, speed);
         movement = transform.TransformDirection(movement);
-        controller.SimpleMove(movement * Time.deltaTime);
+
+        playerVelocity.y = playerVelocity.y + (Physics.gravity.y * customGravity * Time.deltaTime);//add gravity to player's velocity
+        controller.Move(playerVelocity * Time.deltaTime);//apply gravity to character with Move
+
+        controller.Move(movement * Time.deltaTime);
     }
 }
