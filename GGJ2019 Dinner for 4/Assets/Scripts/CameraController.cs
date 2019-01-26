7cc298474b98782c82efba51b9237eeeaaf21b9a
JosephBoudeman <jboudema@ucsc.edu>
2019-01-25 21:00:58 -0800

    Unity
    controller scene

---
+++ GGJ2019 Dinner for 4/Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    GameObject character;
+    Vector2 mouseLook;
+    Vector2 smoothCam;
+    public float sens = 7f;
+    public float smooth = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        character = transform.parent.gameObject;//set this camera as the child of gameobject
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 = 
+    }
+}
