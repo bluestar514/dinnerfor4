847706e56e7bef0f502a237a259fd76d6e7e1e0a
Gram Nitschke <gramnitschke@gmail.com>
2019-01-26 20:27:21 -0800

    Menu Stuff

--- GGJ2019 Dinner for 4/Assets/Scripts/Quit.cs
+++ GGJ2019 Dinner for 4/Assets/Scripts/Quit.cs
@@ -1,9 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class MouseOver : MonoBehaviour
+public class Quit : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,7 +12,11 @@ public class MouseOver : MonoBehaviour
 
     private void OnMouseDown()
     {
-        SceneManager.LoadScene("HouseScene");
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit ();
+        #endif
     }
     // Update is called once per frame
     void Update()
